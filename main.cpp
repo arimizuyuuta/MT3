4779da9147beeb303a217e9c03985f1ec7f762cb
arimizuyuuta <k024g1028@g.neec.ac.jp>
2025-04-22 10:11:18 +0900

    AAA

--- main.cpp
+++ main.cpp
@@ -29,7 +29,7 @@ Vector3 Multiply(float scalar, const Vector3& v) {
 float Dot(const Vector3& v1, const Vector3& v2) {
     return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
 }
-
+ 
 // 長さ（ノルム）
 float Length(const Vector3& v) {
     return sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
